20ed4f7ae738a6b940dfb3a65f7a80caed316c9b
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2022-04-20 17:08:46 +0700

    April 20 Deploy 5

--- routes/api.js
+++ routes/api.js
@@ -362,7 +362,11 @@ function dynamicSort(property) {
 exports.test = async (req, res) => {
   try {
     const client = createClient({
-      url: 'redis://:ped7fa24fe4f7138a5db1b0a6c682e38348d31d120b01754a7168aa1c6f996375@ec2-54-227-24-175.compute-1.amazonaws.com:29620'
+      url: 'redis://:ped7fa24fe4f7138a5db1b0a6c682e38348d31d120b01754a7168aa1c6f996375@ec2-54-227-24-175.compute-1.amazonaws.com:29620',
+      socket: {
+        tls: true,
+        rejectUnauthorized: false
+      }
     });
     client.on('error', (err) => console.log('Redis Client Error', err));
     await client.connect();
--- test.js
+++ test.js
@@ -2,14 +2,18 @@ const redis = require('redis')
 
 ;(async () => {
   const client = redis.createClient({
-    url: 'redis://:ped7fa24fe4f7138a5db1b0a6c682e38348d31d120b01754a7168aa1c6f996375@ec2-54-227-24-175.compute-1.amazonaws.com:29620'
+    url: 'redis://:ped7fa24fe4f7138a5db1b0a6c682e38348d31d120b01754a7168aa1c6f996375@ec2-54-227-24-175.compute-1.amazonaws.com:29620',
+    socket: {
+      tls: true,
+      rejectUnauthorized: false
+    }
   });
 
   client.on('error', (err) => console.log('Redis Client Error', err));
 
   await client.connect();
 
-  await client.set('key', 'value');
+  await client.set('key', 'value1');
   const value = await client.get('key');
   console.log(value);
   client.quit()
