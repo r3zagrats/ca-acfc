b6a6d4d6593f722c5e6939640e32dfebd8962994
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2022-04-22 10:27:05 +0700

    April 22 Deploy

--- routes/activity.js
+++ routes/activity.js
@@ -33,142 +33,158 @@ exports.execute = async (req, res) => {
   console.log('\nContent', Content);
   Content = JSON.parse(Content);
   try {
-    // Query OA Info
-    const { rows } = await db.query(
-      `SELECT * FROM "${process.env.PSQL_ZALOOA_TABLE}" WHERE "OAId" = '${data.inArguments[0].Endpoint}'`
-    );
-    const OAInfo = rows[0];
-    console.log('\nOAInfo: ', OAInfo);
-    let tmpAccessToken = OAInfo.AccessToken || '';
-    // Check if the access token is valid
-    if (IsExpiredToken(Number(OAInfo.Timestamp))) {
-      console.log(`\nAccess Token cua ${OAInfo.OAName} het han`);
-      // Refresh Token
-      let response = await superagent
-        .post(process.env.ZALO_OAUTH_URL)
-        .set('secret_key', process.env.ZALO_APP_SECRET_KEY)
-        .send(`refresh_token=${OAInfo.RefreshToken}`)
-        .send(`app_id=${process.env.ZALO_APP_ID}`)
-        .send('grant_type=refresh_token');
-      response = JSON.parse(response.text);
-      console.log(`\nAccessToken Response cua ${OAInfo.OAName}: "`, response);
-      if (response && response.access_token) {
-        tmpAccessToken = response.access_token;
-        let updateInfo = {
-          ...OAInfo,
-          AccessToken: response.access_token,
-          RefreshToken: response.refresh_token,
-          Timestamp: Date.now() + response.expires_in * 1000,
-          ExpiryDate: new Date(Date.now() + response.expires_in * 1000).toUTCString(),
-        };
-        console.log(`\nupdateInfo cua OA ${OAInfo.OAName}: `, updateInfo);
-        let valueList = [];
-        for (const [key, value] of Object.entries(updateInfo)) {
-          valueList.push(`"${key}" = '${value}'`);
-        }
-        const result = await db.query(
-          `UPDATE "${process.env.PSQL_ZALOOA_TABLE}" SET ${valueList} WHERE "OAId" = '${OAInfo.OAId}'`
+    switch (data.inArguments[0].Channel) {
+      case 'Zalo Notification Service': {
+        // Query OA Info
+        const { rows } = await db.query(
+          `SELECT * FROM "${process.env.PSQL_ZALOOA_TABLE}" WHERE "OAId" = '${data.inArguments[0].Endpoint}'`
         );
-        console.log(`\nCap nhat db thanh cong cho OA ${OAInfo.OAName}:`);
-      } else {
-        throw response;
-      }
-    } else {
-      console.log(`\nAccess Token cua ${OAInfo.OAName} con han`);
-    }
-    console.log('\ntmpAccessToken: ', tmpAccessToken);
-    if (Content.type === 'AttachedFile') {
-      // Check if file exists
-      await redisClient.connect();
-      let fileInfo = await redisClient.get(Content.value.name);
-      fileInfo = JSON.parse(fileInfo);
-      console.log('\nfileInfo: ', fileInfo);
-      let tmpToken = fileInfo.token || '';
-      if (fileInfo === null || IsExpiredToken(fileInfo.expires_in) === true || !fileInfo.token) {
-        const result = await asyncGet(Content.value.url, Content.value.name);
-        console.log('\nresult: ', result);
-        const file = fs.createReadStream(`./public/data/${Content.value.name}`);
-        const response = await superagent
-          .post(
-            `${process.env.ZALO_UPLOAD_URL}${Content.value.extension === 'gif' ? 'gif' : 'file'}`
-          )
-          .set('access_token', tmpAccessToken)
-          .set('content-type', 'multipart/form-data')
-          .field('file', file);
-        console.log('\nresponse', response.body);
-        if (response.body.error === 0) {
-          tmpToken = response.body.data.token || response.body.data.attachment_id;
+        const OAInfo = rows[0];
+        console.log('\nOAInfo: ', OAInfo);
+        let tmpAccessToken = OAInfo.AccessToken || '';
+        // Check if the access token is valid
+        if (IsExpiredToken(Number(OAInfo.Timestamp))) {
+          console.log(`\nAccess Token cua ${OAInfo.OAName} het han`);
+          // Refresh Token
+          let response = await superagent
+            .post(process.env.ZALO_OAUTH_URL)
+            .set('secret_key', process.env.ZALO_APP_SECRET_KEY)
+            .send(`refresh_token=${OAInfo.RefreshToken}`)
+            .send(`app_id=${process.env.ZALO_APP_ID}`)
+            .send('grant_type=refresh_token');
+          response = JSON.parse(response.text);
+          console.log(`\nAccessToken Response cua ${OAInfo.OAName}: "`, response);
+          if (response && response.access_token) {
+            tmpAccessToken = response.access_token;
+            let updateInfo = {
+              ...OAInfo,
+              AccessToken: response.access_token,
+              RefreshToken: response.refresh_token,
+              Timestamp: Date.now() + response.expires_in * 1000,
+              ExpiryDate: new Date(Date.now() + response.expires_in * 1000).toUTCString(),
+            };
+            console.log(`\nupdateInfo cua OA ${OAInfo.OAName}: `, updateInfo);
+            let valueList = [];
+            for (const [key, value] of Object.entries(updateInfo)) {
+              valueList.push(`"${key}" = '${value}'`);
+            }
+            const result = await db.query(
+              `UPDATE "${process.env.PSQL_ZALOOA_TABLE}" SET ${valueList} WHERE "OAId" = '${OAInfo.OAId}'`
+            );
+            console.log(`\nCap nhat db thanh cong cho OA ${OAInfo.OAName}:`);
+          } else {
+            throw response;
+          }
         } else {
-          throw response.body.message;
+          console.log(`\nAccess Token cua ${OAInfo.OAName} con han`);
         }
-        await redisClient.set(
-          Content.value.name,
+        console.log('\ntmpAccessToken: ', tmpAccessToken);
+        if (Content.type === 'AttachedFile') {
+          // Check if file exists
+          await redisClient.connect();
+          let fileInfo = await redisClient.get(Content.value.name);
+          fileInfo = JSON.parse(fileInfo);
+          console.log('\nfileInfo: ', fileInfo);
+          let tmpToken = fileInfo.token || '';
+          if (
+            fileInfo === null ||
+            IsExpiredToken(fileInfo.expires_in) === true ||
+            !fileInfo.token
+          ) {
+            const result = await asyncGet(Content.value.url, Content.value.name);
+            console.log('\nresult: ', result);
+            const file = fs.createReadStream(`./public/data/${Content.value.name}`);
+            const response = await superagent
+              .post(
+                `${process.env.ZALO_UPLOAD_URL}${
+                  Content.value.extension === 'gif' ? 'gif' : 'file'
+                }`
+              )
+              .set('access_token', tmpAccessToken)
+              .set('content-type', 'multipart/form-data')
+              .field('file', file);
+            console.log('\nresponse', response.body);
+            if (response.body.error === 0) {
+              tmpToken = response.body.data.token || response.body.data.attachment_id;
+            } else {
+              throw response.body.message;
+            }
+            await redisClient.set(
+              Content.value.name,
+              JSON.stringify({
+                token: tmpToken,
+                expires_in: Date.now() + 604800000,
+              })
+            );
+          }
+          console.log('\ntmpToken:', tmpToken);
+          if (Content.value.extension === 'gif') {
+            Content.payloadData.message.attachment.payload.elements[0].attachment_id = tmpToken;
+          } else {
+            Content.payloadData.message.attachment.payload.token = tmpToken;
+          }
+          await redisClient.quit();
+        }
+        let znsContent = Content.payloadData;
+        console.log('\nznsContent:', JSON.stringify(znsContent));
+        // Send Message
+        const response = await superagent
+          .post('https://openapi.zalo.me/v2.0/oa/message')
+          .set('Content-Type', 'application/json')
+          .set('access_token', tmpAccessToken)
+          .send(JSON.stringify(znsContent));
+        console.log('\nResponse data:', response.body);
+        const znsSendLog = response.body;
+        console.log('\nznsSendLog:', znsSendLog);
+        if (znsSendLog.error !== 0) throw znsSendLog.message;
+        const temp = {
+          MsgId: znsSendLog.error === 0 ? znsSendLog.data.message_id : '',
+          ZaloId: znsSendLog.error === 0 ? znsSendLog.data.user_id : '',
+          UTCTime: new Date().toUTCString(),
+          Timestamp: new Date().getTime(),
+          Error: znsSendLog.error,
+          Message: znsSendLog.message,
+        };
+        const firstStep = await RestClient.insertZaloSendLog(
           JSON.stringify({
-            token: tmpToken,
-            expires_in: Date.now() + 604800000,
+            items: [temp],
           })
         );
+        res.status(200).send({ Status: 'Successfull' });
+        break;
       }
-      console.log('\ntmpToken:', tmpToken);
-      if (Content.value.extension === 'gif') {
-        Content.payloadData.message.attachment.payload.elements[0].attachment_id = tmpToken;
-      } else {
-        Content.payloadData.message.attachment.payload.token = tmpToken;
+      case 'Web Push Notification': {
+        // console.log('\nWebpush method');
+        // let FirebaseToken = data.inArguments[0].FirebaseToken;
+        // if (FirebaseToken !== '') {
+        //   var payload = {
+        //     notification: JSON.parse(Content),
+        //   };
+        //   admin
+        //     .messaging()
+        //     .sendToDevice(FirebaseToken, payload)
+        //     .then((response) => {
+        //       console.log('\nSent successfully.\n');
+        //       console.log(response);
+        //     })
+        //     .catch((error) => {
+        //       console.log('\nSent failed.\n');
+        //       console.log(error);
+        //     });
+        // }
+        console.log('\nThis is Web Push Notification Channel.');
+        res.status(200).send({ Status: 'Accept' });
+        break;
       }
-      await redisClient.quit();
+      case 'SMS': {
+        console.log('\nThis is SMS Channel.');
+        res.status(200).send({ Status: 'Accept' });
+        break;
+      }
+      default: {
+        throw 'This Channel is not supported';
+      }
     }
-    let znsContent = Content.payloadData;
-    console.log('\nznsContent:', JSON.stringify(znsContent));
-    // Send Message
-    const response = await superagent
-      .post('https://openapi.zalo.me/v2.0/oa/message')
-      .set('Content-Type', 'application/json')
-      .set('access_token', tmpAccessToken)
-      .send(JSON.stringify(znsContent));
-    console.log('\nResponse data:', response.body);
-    const znsSendLog = response.body;
-    console.log('\nznsSendLog:', znsSendLog);
-    if (znsSendLog.error !== 0) throw znsSendLog.message;
-    const temp = {
-      MsgId: znsSendLog.error === 0 ? znsSendLog.data.message_id : '',
-      ZaloId: znsSendLog.error === 0 ? znsSendLog.data.user_id : '',
-      UTCTime: new Date().toUTCString(),
-      Timestamp: new Date().getTime(),
-      Error: znsSendLog.error,
-      Message: znsSendLog.message,
-    };
-    const firstStep = await RestClient.insertZaloSendLog(
-      JSON.stringify({
-        items: [temp],
-      })
-    );
-    res.status(200).send({ Status: 'Successfull' });
-    // case 'Webpush': {
-    //   console.log('\nWebpush method');
-    //   let FirebaseToken = data.inArguments[0].FirebaseToken;
-    //   if (FirebaseToken !== '') {
-    //     for (const [key, value] of Object.entries(data.inArguments[0])) {
-    //       Content = Content.replaceAll(`%%${key}%%`, value);
-    //     }
-    //     var payload = {
-    //       notification: JSON.parse(Content),
-    //     };
-    //     admin
-    //       .messaging()
-    //       .sendToDevice(FirebaseToken, payload)
-    //       .then((response) => {
-    //         console.log('\nSent successfully.\n');
-    //         console.log(r\nesponse);
-    //       })
-    //       .catch((error) => {
-    //         console.log('\nSent failed.\n');
-    //         console.log(e\nrror);
-    //       });
-    //   }
-    //   res.status(200).send({ Status: 'Accept' });
-    //   break;
-    // }
-    // }
   } catch (error) {
     res.status(500).send({ status: 'Error', message: error });
   }
