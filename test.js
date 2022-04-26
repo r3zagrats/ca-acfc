3675dd79b04f0feb672956bec1c2b8d682def338
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2022-04-26 16:40:41 +0700

    26 april deploy

--- test.js
+++ test.js
@@ -1,8 +1,2 @@
-const redisClient = require('./db/redis')
-
-;(async () => {
-  await redisClient.connect();
-  const result = await redisClient.get('key');
-  console.log(result);
-  await redisClient.quit()
-})()
+const crypto = require('crypto');
+console.log(crypto.randomBytes(16).toString('base64'))
