08dbfc6500efe2e9f91d7d750a1d8cc663201a71
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2022-05-17 09:19:31 +0700

    May 16 Deploy

--- routes/zalowh.js
+++ routes/zalowh.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const RestClient = require('../utils/sfmc-client');
 const pgdb = require('../db/postgresql');
 const superagent = require('superagent');
+const { refreshZaloAT } = require('../utils/refreshZaloAT');
 
 exports.zaloWebhook = async (req, res) => {
   const userTrackingInfo = req.body;
@@ -70,6 +71,7 @@ exports.zaloWebhook = async (req, res) => {
     case 'follow': {
       console.log('User follow OA');
       try {
+        const response = await superagent
         const insertData = RestClient.insertZaloUserActionTracking(
           JSON.stringify({
             items: [
@@ -191,10 +193,8 @@ exports.zaloWebhook = async (req, res) => {
             })
           );
         }
-
         try {
           input = JSON.parse(input);
-
           if (input.EventDefinitionKey) {
             console.log('User triggered journey');
             const result = await RestClient.triggerJourneyBuilder(
@@ -210,49 +210,7 @@ exports.zaloWebhook = async (req, res) => {
             console.log('result:', result);
           }
         } catch (error) {}
-
-        const { rows } = await pgdb.query(
-          `SELECT * FROM "${process.env.PSQL_ZALOOA_TABLE}" WHERE "OAId" = '${userTrackingInfo.recipient.id}'`
-        );
-        const OAInfo = rows[0];
-        console.log('\nOAInfo: ', OAInfo);
-        let tmpAccessToken = OAInfo.AccessToken || '';
-        // Check if the access token is valid
-        if (IsExpiredToken(Number(OAInfo.Timestamp))) {
-          console.log(`\nAccess Token cua ${OAInfo.OAName} het han`);
-          // Refresh Token
-          let response = await superagent
-            .post(process.env.ZALO_OAUTH_URL)
-            .set('secret_key', process.env.ZALO_APP_SECRET_KEY)
-            .send(`refresh_token=${OAInfo.RefreshToken}`)
-            .send(`app_id=${process.env.ZALO_APP_ID}`)
-            .send('grant_type=refresh_token');
-          response = JSON.parse(response.text);
-          console.log(`\nAccessToken Response cua ${OAInfo.OAName}: "`, response);
-          if (response && response.access_token) {
-            tmpAccessToken = response.access_token;
-            let updateInfo = {
-              ...OAInfo,
-              AccessToken: response.access_token,
-              RefreshToken: response.refresh_token,
-              Timestamp: Date.now() + response.expires_in * 1000,
-              ExpiryDate: new Date(Date.now() + response.expires_in * 1000).toUTCString(),
-            };
-            console.log(`\nupdateInfo cua OA ${OAInfo.OAName}: `, updateInfo);
-            let valueList = [];
-            for (const [key, value] of Object.entries(updateInfo)) {
-              valueList.push(`"${key}" = '${value}'`);
-            }
-            const result = await pgdb.query(
-              `UPDATE "${process.env.PSQL_ZALOOA_TABLE}" SET ${valueList} WHERE "OAId" = '${OAInfo.OAId}'`
-            );
-            console.log(`\nCap nhat db thanh cong cho OA ${OAInfo.OAName}:`);
-          } else {
-            throw response;
-          }
-        } else {
-          console.log(`\nAccess Token cua ${OAInfo.OAName} con han`);
-        }
+        const tmpAccessToken = await refreshZaloAT(userTrackingInfo.recipient.id);
         console.log('\ntmpAccessToken: ', tmpAccessToken);
 
         let znsContent = {
@@ -304,10 +262,3 @@ exports.zaloWebhook = async (req, res) => {
   }
 };
 
-const IsExpiredToken = (timestamp) => {
-  console.log('\nCurrent Time: ', new Date(Date.now()).toUTCString());
-  console.log('Expired Time: ', new Date(timestamp).toUTCString());
-  console.log(timestamp - Date.now());
-  if (timestamp - Date.now() < 600000) return true;
-  else return false;
-};
