7ea435db3e904b77a81a72697683bec05171738c
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2022-05-17 10:50:10 +0700

    May 16 Deploy

--- test.js
+++ test.js
@@ -1,7 +1,1 @@
-const superagent = require('superagent');
-(async () => {
-  const token = await superagent
-    .post('http://localhost:8080/api/getznstemplatedetail')
-    .send({ TemplateId: '225285', OAId: '1015448571833111846' });
-  console.log('token:', token.text);
-})();
+console.log(Date.now());
