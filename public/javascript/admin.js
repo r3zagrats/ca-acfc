468f17b654e3373eca735d8288b3bc26e241e3c9
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2022-05-18 10:05:31 +0700

    May 16 Deploy

---
+++ public/javascript/admin.js
@@ -0,0 +1,17 @@
+// $(document).ready(async () => {
+//   const data = await fetchMenu();
+//   renderMenu(data.data);
+//   console.log(data.data);
+// });
+
+// const fetchMenu = async () => {
+//   const menuData = await superagent.get('/pgdb/channels');
+//   return menuData.body;
+// };
+
+// const renderMenu = (menuList) => {
+//   $.each(menuList, (index, item) => {
+//     $('.menu').append(`<li class="menu-item">${item.Name}
+//     </li>`);
+//   });
+// };
