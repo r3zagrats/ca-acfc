23d786c89d6822e611e16ef6e7c23e9d0f53171e
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2022-05-24 16:29:57 +0700

    May 24 deploy

--- routes/sfmcca.routes.js
+++ routes/sfmcca.routes.js
@@ -3,10 +3,10 @@
 const router = require('express').Router();
 const SFMCCAController = require('../controllers/sfmcca.controllers');
 
-router.use('/execute', SFMCCAController.execute);
-router.use('/save', SFMCCAController.save);
-router.use('/publish', SFMCCAController.publish);
-router.use('/validate', SFMCCAController.validate);
-router.use('/stop', SFMCCAController.stop);
+router.use('journey/execute', SFMCCAController.execute);
+router.use('journey/save', SFMCCAController.save);
+router.use('journey/publish', SFMCCAController.publish);
+router.use('journey/validate', SFMCCAController.validate);
+router.use('journey/stop', SFMCCAController.stop);
 
 module.exports = router;
