4b8f11cc03d8c9a11e6a7aef6f4725956ad9ea97
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2022-06-10 14:59:20 +0700

    June 9 Deploy

--- api/zalo/index.js
+++ api/zalo/index.js
@@ -1,15 +1,14 @@
 const superagent = require('superagent');
-const refreshZaloToken = require('../../services/zalo/refreshZaloToken');
 
 const ZaloAPI = {
   getZNSTemplates: async (req, res) => {
     try {
-      console.log('req body:', req.body.OAId);
-      const token = await refreshZaloToken(req.body.OAId);
-      console.log('token:', token);
       const response = await superagent
-        .get('https://business.openapi.zalo.me/template/all?offset=0&limit=100&status=1')
-        .set('access_token', token);
+        .get(
+          `https://cloud.vietguys.biz:4438/api/zalo/v1/template/all?offset=0&limit=100&username=${process.env.ACFC_ZNS_USERNAME}&oa_id=${req.body.OAId}`
+        )
+        .set('Authorization', `Bearer ${process.env.ACFC_ZNS_TOKEN}`);
+      console.log('response:', response.text);
       res.status(200).send(response.text);
     } catch (error) {
       res.status(500).send(error.message);
@@ -18,15 +17,11 @@ const ZaloAPI = {
 
   getZNSTemplateDetail: async (req, res) => {
     try {
-      console.log(
-        'req body:',
-        `https://business.openapi.zalo.me/template/info?template_id=${req.body.TemplateId}`
-      );
-      const token = await refreshZaloToken(req.body.OAId);
-      console.log('token:', token);
       const response = await superagent
-        .get(`https://business.openapi.zalo.me/template/info?template_id=${req.body.TemplateId}`)
-        .set('access_token', token);
+        .get(
+          `https://cloud.vietguys.biz:4438/api/zalo/v1/template/info?template_id=${req.body.TemplateId}&username=${process.env.ACFC_ZNS_USERNAME}&oa_id=${req.body.OAId}`
+        )
+        .set('Authorization', `Bearer ${process.env.ACFC_ZNS_TOKEN}`);
       console.log('response:', response.text);
       res.status(200).send(response.text);
     } catch (error) {
@@ -36,8 +31,8 @@ const ZaloAPI = {
 
   getAuthCode: async (req, res) => {
     console.log(req.body);
-    res.status(200).send(req.body)
-  } 
+    res.status(200).send(req.body);
+  },
 };
 
 module.exports = ZaloAPI;
