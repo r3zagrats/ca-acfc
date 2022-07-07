e2659a2401cc29e737af6ec67e5594ad71aa3e10
r3zagrats <91927573+r3zagrats@users.noreply.github.com>
2022-07-07 15:26:57 +0700

    July 7 deploy

--- public/javascript/sfmcca.js
+++ public/javascript/sfmcca.js
@@ -84,7 +84,7 @@ const onRender = () => {
         $('.ca-modal').hide();
         customContent = JSON.parse(customContent);
         console.log('customContent:', customContent);
-        if (customContent.resultCode !== 0) throw customContent.message;
+        if (customContent.resultCode != 0) throw customContent.resultDesc;
         connection.trigger('updateButton', {
           button: 'next',
           enabled: true,
